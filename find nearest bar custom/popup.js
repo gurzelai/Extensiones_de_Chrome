0aae500b73bd0f4777d864264084b6ea587a5de6
Gorka Urzelai <gorkaurzelai2000@gmail.com>
2023-10-26 14:57:14 +0200

    find nearest bar custom

---
+++ find nearest bar custom/popup.js
@@ -0,0 +1,11 @@
+chrome.storage.sync.get(['tipoLugar'], function(result) {
+    let lugar = result.tipoLugar || 'mcdonalds';
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        const latitud = position.coords.latitude;
+        const longitud = position.coords.longitude;
+        const url = `https://www.google.com/maps/search/${lugar}/@${latitud},${longitud}`;
+        chrome.tabs.create({ url: url });
+    });
+
+});
