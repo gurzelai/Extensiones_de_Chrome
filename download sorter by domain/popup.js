dcd0c91ee2b216d4572dfbfdbd39afb36b5ca066
Gorka Urzelai <gorkaurzelai2000@gmail.com>
2023-10-26 09:48:21 +0200

    Show status in popup

--- download sorter by domain/popup.js
+++ download sorter by domain/popup.js
@@ -1,28 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleCheckbox = document.getElementById('toggleCheckbox');
     const statusElement = document.getElementById('status');
+    
+    console.log("AAAAAAAAAAAA")
 
-    chrome.storage.local.get(['extensionEnabled'], function(result) {
-        const isEnabled = result.extensionEnabled !== undefined ? result.extensionEnabled : true;
+    chrome.storage.local.get('status', function(result) {
+        const isEnabled = result.status !== undefined ? result.status : false;
 
         if (isEnabled) {
-            toggleCheckbox.checked = true;
             statusElement.textContent = 'Extension is currently active.';
         } else {
-            toggleCheckbox.checked = false;
             statusElement.textContent = 'Extension is currently inactive.';
         }
-
-        toggleCheckbox.addEventListener('change', function() {
-            const isChecked = toggleCheckbox.checked;
-
-            if (isChecked) {
-                statusElement.textContent = 'Extension is currently active.';
-            } else {
-                statusElement.textContent = 'Extension is currently inactive.';
-            }
-
-            chrome.storage.local.set({ extensionEnabled: isChecked });
-        });
     });
 });
